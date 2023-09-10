15e942d58569c02c8a200d2104849391ef0eb6bf
Abdulhameed Yunusa <abdulhameed179@outlook.com>
2023-09-10 15:23:39 +0100

    starting files

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require('cors')
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// enable cors
+app.use(cors())
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+const MongoDB_CONNECTION_URL = process.env.MONGODB_CONNECTION_URL;
+
+mongoose
+    .connect(MongoDB_CONNECTION_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Connection to MongoDB successful");
+    })
+    .catch((error) => {
+        console.error("MongoDB Connection error:", error);
+    });
+
+const Person = new mongoose.Schema({
+    Name: {
+        type: String,
+        required: true,
+    },
+});
